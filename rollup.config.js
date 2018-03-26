4c5a18398016884f56eaa190cbd46dcc389fa5e0
Pat Cavit <github@patcavit.com>
2018-03-26 09:23:26 -0700

    Multiple outputs instead of multiple bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,19 @@
 import svelte from 'rollup-plugin-svelte';
 
 export default [
-	// ES module version, for modern browsers
 	{
 		input: ['src/main-a.js', 'src/main-b.js'],
-		output: {
+		output: [{
+			// ES module version, for modern browsers
 			dir: 'public/module',
 			format: 'es',
 			sourcemap: true
-		},
-		plugins: [svelte()],
-		experimentalCodeSplitting: true,
-		experimentalDynamicImport: true
-	},
-
-	// SystemJS version, for older browsers
-	{
-		input: ['src/main-a.js', 'src/main-b.js'],
-		output: {
+		}, {
+			// SystemJS version, for older browsers
 			dir: 'public/nomodule',
 			format: 'system',
 			sourcemap: true
-		},
+		}],
 		plugins: [svelte()],
 		experimentalCodeSplitting: true,
 		experimentalDynamicImport: true
